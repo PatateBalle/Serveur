80aa54e258202af86656d757aad7cdba10eaa328
PatateBalle <ewanpro2008@gmail.com>
2022-09-03 23:54:43 +0200

    html

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
     try 
     {
-        $bdd = new PDO('mysql:host=localhost;dbname=mylevel;charset=utf8', 'root','');
+        $bdd = new PDO('mysql:host=http://78.115.223.153/;dbname=mylevel;charset=utf8', 'ewanprnt','0');
     }catch(PDOException $e)
     {
         die('Erreur : '.$e->getMessage());
